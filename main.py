5383bd9d698c545167d4097812f006ecd30bcaa9
Niclas Bångman <kingkong.salcin@gmail.com>
2019-01-28 14:00:59 +0100

    add main

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+class WordCount():
+	counter = {}
+	
+	def __init__(word):
+		pass
+
+
+
+def main():
+	with open('pride-and-prejudice.txt', 'r') as text_file:
+		for line in text_file.readlines():
+
+
+
+if __name__ == 'main':
+	main()
